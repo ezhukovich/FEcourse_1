a03674ee3520937a1a28ce2a8eb1627e74a56f79
Vadym Kostiuk <vadim.dk81@gmail.com>
2020-06-16 12:36:15 +0300

    L28_Classes__HW

--- Kostiuk/homework/L28-JS__Classes/homework.js
+++ Kostiuk/homework/L28-JS__Classes/homework.js
@@ -53,3 +53,57 @@ c.calcSquare();
 c.calcLength();
 
 console.log(c.diametr);
+
+
+// 2  2) Реализовать класс, описывающий простой маркер. В классе должны быть следующие компоненты:
+
+// поле, которое хранит цвет маркера;
+// поле, которое хранит количество чернил в маркере(в процентах);
+// метод для печати(метод принимает строку и выводит текст соответствующим цветом; текст выводится до тех пор, пока в маркере есть чернила; один не пробельный символ – это 0, 5 % чернил в маркере).
+// Реализовать класс, описывающий заправляющийся маркер, унаследовав его от простого маркера и добавив метод для заправки маркера.
+
+// Продемонстрировать работу написанных методов. 
+
+class Marker {
+    constructor(_color, _ink, _string) {
+        this.color = _color;
+        this.ink = _ink;
+        this.string = _string
+
+
+    }
+
+    writeText(){
+        let marker = document.getElementById('marker');
+        // console.log(this.string.length);
+        let symbolsCanPrint = this.ink / 0.5; 
+        let arr = this.string.split('');
+        // console.log('symbols:', symbolsCanPrint);
+        // console.log(arr);
+
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[j] == ' '){
+                var newsymbolsCanPrint = ++symbolsCanPrint;
+            }
+        }
+       
+        if (symbolsCanPrint > this.string.length) {
+            marker.innerHTML = this.string;
+            marker.style.color = this.color;
+
+        }else{
+            for (let i = 0; i < newsymbolsCanPrint; i++) {
+                let span = document.createElement('span');
+                span.innerHTML = arr[i];
+                marker.appendChild(span);
+                marker.style.color = this.color;
+            }
+        }
+
+
+    }
+}
+
+let m = new Marker("orange", 5, "Hello da dadba");
+
+m.writeText();
